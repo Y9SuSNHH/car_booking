20877e9a408a1763388ac0e62845eb30ab600adc
Y9SusNHH <Y9SusNHH@gmail.com>
2022-07-03 17:45:51 +0700

    create route role

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +14,16 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
+Route::get('/', function() {
     return view('welcome');
-});
+})->name('welcome');
+
+Route::get('/signin', [AuthController::class, 'signin'])->name('signin');
+Route::post('/signin', [AuthController::class, 'processSignIn'])->name('process.signin');
+Route::get('/signup', [AuthController::class, 'signup'])->name('signup');
+Route::post('/signup', [AuthController::class, 'processSignUp'])->name('process.signup');
+Route::get('/signout', [AuthController::class, 'signout'])->name('signout');
+Route::get('/auth/redirect/{provider}', function($provider) {
+    return Socialite::driver($provider)->redirect();
+})->name('auth.redirect');
+Route::get('/auth/callback/{provider}', [AuthController::class, 'callback'])->name('auth.callback');
