dfa0dfabd26db007f7d7f0b5baf026fa844053b5
Nguyễn Huy Hùng <y9susnhh@gmail.com>
2022-09-04 12:26:31 +0700

    update route user

--- app/Http/Controllers/User/BillController.php
+++ app/Http/Controllers/User/BillController.php
@@ -11,13 +11,23 @@ use App\Models\Car;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 
-class HomePageController extends Controller
+class BillController extends Controller
 {
 
-    public function storeBill(Request $request, $carId)
+    /**
+     * @param Request $request
+     * @param $carId
+     * @return RedirectResponse
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public function store(Request $request, $carId): \Illuminate\Http\RedirectResponse
     {
         $bill = Bill::create([
             "user_id"     => auth()->user()->id,
--- routes/user.php
+++ routes/user.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\User\HomePageController;
+use App\Http\Controllers\User\BillController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function() {
     return redirect()->route('welcome');
 })->name('welcome');
 
-Route::post('/bill/create/{carId?}', [HomePageController::class, 'storeBill'])->name('bill.store');
+Route::post('/bill/create/{carId?}', [BillController::class, 'store'])->name('bill.store');
