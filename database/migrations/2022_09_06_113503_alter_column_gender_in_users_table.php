e60681370235c8cfa4a119d3d921257d7818b964
Nguyễn Huy Hùng <y9susnhh@gmail.com>
2022-09-07 00:35:34 +0700

    Create 2022_09_06_113503_alter_column_gender_in_users_table.php

---
+++ database/migrations/2022_09_06_113503_alter_column_gender_in_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterColumnGenderInUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasColumn('users', 'gender')) {
+            Schema::table('users', function(Blueprint $table) {
+                $table->boolean('gender')->default(1)->change();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
